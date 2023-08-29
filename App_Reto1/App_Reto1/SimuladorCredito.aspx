fd5934b336a80492553afbfafb3b682aa7ef879e
JoseP <jomapassos2001@gmail.com>
2023-08-29 07:57:56 -0500

    2do commit diseño credito

---
+++ App_Reto1/App_Reto1/SimuladorCredito.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SimuladorCredito.aspx.cs" Inherits="App_Reto1.SimuladorCredito" %>
+
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Registro de Empleados</title>
+    <link rel="stylesheet" href="estilos/botones.css" />
+    <link rel="stylesheet" href="estilos/cajas.css" />
+    <link rel="stylesheet" href="estilos/div.css" />
+    <link rel="stylesheet" href="estilos/labels.css" />
+    <link rel="stylesheet" href="estilos/layout.css" />
+</head>
+
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div class="label-container">
+                <span class="label">Id:</span>
+                <asp:TextBox ID="txtId" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <div class="label-container">
+                <span class="label">Nombres:</span>
+                <asp:TextBox ID="txtNombres" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <div class="label-container">
+                <span class="label">Apellidos:</span>
+                <asp:TextBox ID="txtApellidos" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <div class="label-container">
+                <span class="label">Contacto:</span>
+                <asp:TextBox ID="txtContacto" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <div class="label-container">
+                <span class="label">Correo:</span>
+                <asp:TextBox ID="txtCorreo" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <div class="label-container">
+                <span class="label">Dirección:</span>
+                <asp:TextBox ID="txtDireccion" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <div class="label-container">
+                <span class="label">Salario:</span>
+                <asp:TextBox ID="txtSalario" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <div class="label-container">
+                <span class="label">Empresa:</span>
+                <asp:TextBox ID="txtEmpresa" runat="server" CssClass="input-box"></asp:TextBox>
+            </div>
+
+            <asp:Button ID="btnGuardar" runat="server" Text="Guardar" CssClass="btn" />
+        </div>
+    </form>
+</body>
+
+</html>
+            <%--<asp:Button ID="btnGuardar" runat="server" Text="Guardar" OnClick="btnGuardar_Click" />--%>
---
+++ App_Reto1/App_Reto1/SimuladorCredito.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace App_Reto1
+{
+
+
+    public partial class SimuladorCredito
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control txtId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtId;
+
+        /// <summary>
+        /// Control txtNombres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombres;
+
+        /// <summary>
+        /// Control txtApellidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellidos;
+
+        /// <summary>
+        /// Control txtContacto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtContacto;
+
+        /// <summary>
+        /// Control txtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+
+        /// <summary>
+        /// Control txtDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDireccion;
+
+        /// <summary>
+        /// Control txtSalario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSalario;
+
+        /// <summary>
+        /// Control txtEmpresa.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmpresa;
+
+        /// <summary>
+        /// Control btnGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGuardar;
+    }
+}
